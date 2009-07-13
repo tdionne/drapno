7f598f2b67bfd60332677bff93bc4e84784848ee
James Stewart <james@saigon.local>
2009-07-13 16:29:53 +0100

    Move test dependencies to relevant environment file

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
-  config.gem 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
   config.gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
   config.gem 'thoughtbot-clearance', :lib => 'clearance', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'freelancing-god-thinking-sphinx', :lib => 'thinking_sphinx', :version => '1.1.23'
