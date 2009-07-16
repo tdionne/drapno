6cc7e54ff2b756489e12f53550778b009c28bef5
James Stewart <james@saigon.local>
2009-07-16 11:05:52 +0100

    don't try and create empty appearances

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,7 +1,8 @@
 class Dream < ActiveRecord::Base
   belongs_to :dreamer
   has_many :appearances
-  accepts_nested_attributes_for :appearances
+  accepts_nested_attributes_for :appearances, :reject_if => proc { |attributes| attributes['name'].blank? || attributes['email'].blank? }
+  
   
   validates_presence_of :title
   validates_presence_of :story
