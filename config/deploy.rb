7ba5bf20cdae367b46ff67428baaecadd357891e
James Stewart <james@saigon.local>
2009-07-17 20:29:40 +0100

    add staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,14 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "drapno"
+set :repository,  "git@github.com:jystewart/drapno.git"
+set :rails_env, "staging"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{rails_env}.#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :branch, "master"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "212.124.198.2"
+role :web, "212.124.198.2"
+role :db,  "212.124.198.2", :primary => true
