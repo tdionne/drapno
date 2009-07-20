d3933d80da0c8f2ad2234322ccbf1aad73f0ca61
James Stewart <james@saigon.local>
2009-07-20 16:46:22 +0100

    exception notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   include ErrorRenderers
   include InvitationSystem
+  include ExceptionNotifiable
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,3 @@
+ExceptionNotifier.exception_recipients = %w(james@ketlai.co.uk)
+ExceptionNotifier.sender_address = %("Drapno Error" <app.error@drapno.remote>)
+ExceptionNotifier.email_prefix = "[Drapno] "
