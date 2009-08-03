68fcadaf400ea6629ddbaae59e3a0fc92f2cd127
James Stewart <james@ketlai.co.uk>
2009-08-03 15:32:09 +0100

    just use the one method to update cached attributes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -11,7 +11,7 @@
 #
 
 class Rating < ActiveRecord::Base
-  belongs_to :rater, :class_name => 'Dreamer', :counter_cache => true
+  belongs_to :rater, :class_name => 'Dreamer'
   belongs_to :dream, :counter_cache => true
   
   validates_presence_of :rater_id
