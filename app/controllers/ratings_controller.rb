df7782e7531cdc892ce55e31ed7c50b6276dd319
James Stewart <james@ketlai.co.uk>
2009-09-15 12:23:44 +0100

    disambiguate

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,7 +6,7 @@ class RatingsController < ApplicationController
   # GET /ratings.xml
   def index
     @dreams = Dream.listings.paginate :per_page => 15, :page => params[:page], :order => ['average_rating DESC'], 
-      :conditions => 'ratings_count > 0'
+      :conditions => 'dreams.ratings_count > 0'
   end
 
   # POST /ratings
