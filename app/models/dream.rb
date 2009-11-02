eab390a5d14fb29c3a84839aaec8bfcb5b6f3d1e
James Stewart <james@ketlai.co.uk>
2009-11-02 16:17:03 +0000

    Prevent users rating their own dreams

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -55,6 +55,10 @@ class Dream < ActiveRecord::Base
     dreamer.followers
   end
   
+  def can_rate?(dream)
+    dream.dreamer != self
+  end
+  
   include ActivityMonitor
   monitor_activity :object_details => proc { |dream| [dream.id, dream.title] },
     :verb => 'shared',
