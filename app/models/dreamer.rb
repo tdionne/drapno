89a1b6a78f4299e919a3aab4c2753d0dcfc9793f
James Stewart <james@ketlai.co.uk>
2009-11-19 19:31:46 +0000

    Tidy up the dreamer view to make it a bit more interesting - show avatar, etc.

--- app/models/dreamer.rb
+++ app/models/dreamer.rb
@@ -53,7 +53,7 @@ class Dreamer < ActiveRecord::Base
     end
   end
   
-  has_attached_file :picture, :styles => { :thumb => "27x27#" }, :default_url => ":random_default_url"
+  has_attached_file :picture, :styles => { :regular => '180x180#', :thumb => "27x27#" }, :default_url => ":random_default_url"
 
   validates_attachment_content_type :picture, :content_type => /^image\/.*/
   
