33c04562a5178b202b3b060ae39a6ca34c54c516
James Stewart <james@ketlai.co.uk>
2009-11-26 16:11:57 +0000

    Updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,12 @@
+require 'capistrano/ext/multistage'
+require 'delayed/recipes'
+
+set :default_stage, "staging"
+
 set :application, "drapno"
 set :repository,  "git@github.com:jystewart/drapno.git"
-set :rails_env, "staging"
 set :user, "drapno"
 
-set :deploy_to, "/var/www/#{rails_env}.#{application}"
 set :use_sudo, false
 set :deploy_via, :remote_cache
 
@@ -19,24 +22,6 @@ role :db,  "212.124.198.2", :primary => true
 desc "Link in the production configuration"
 task :link_config do
   run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-end
-
-# add this to config/deploy.rb
-namespace :delayed_job do
-  desc "Start delayed_job process" 
-  task :start, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start" 
-  end
-
-  desc "Stop delayed_job process" 
-  task :stop, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop" 
-  end
-
-  desc "Restart delayed_job process" 
-  task :restart, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart" 
-  end
 end
 
 after "deploy:start", "delayed_job:start" 
