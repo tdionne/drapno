6333b4586709699526ac92b0467e46eee96bf093
James Stewart <james@ketlai.co.uk>
2009-12-02 14:52:22 +0000

    The usual thing. Forgot to delete initializer after removing exception notifier

--- config/initializers/exception_notifier.rb
+++
@@ -1,3 +0,0 @@
-ExceptionNotifier.exception_recipients = %w(james@ketlai.co.uk)
-ExceptionNotifier.sender_address = %("Drapno Error" <app.error@drapno.remote>)
-ExceptionNotifier.email_prefix = "[Drapno] "
