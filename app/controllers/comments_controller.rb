9f2298363562596217ab6b8e8890015eae3ecea3
James Stewart <james@ketlai.co.uk>
2010-02-15 08:38:28 +0000

    Fix https://jystewart.hoptoadapp.com/errors/1306543 - Make it explicit that it's the dreams.comments_count field we're sorting by

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,8 @@ class CommentsController < ApplicationController
   before_filter :load_dream, :except => :index
   
   def index
-    @dreams = Dream.listings.paginate :per_page => 15, :page => params[:page], :order => ['comments_count DESC'],
-      :conditions => 'comments_count > 0'
+    @dreams = Dream.listings.paginate :per_page => 15, :page => params[:page], :order => ['dreams.comments_count DESC'],
+      :conditions => 'dreams.comments_count > 0'
   end
   
   def create
