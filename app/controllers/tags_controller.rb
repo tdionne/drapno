7634a8e6fbdbdbc01c7bbb3551f32c3d78914755
James Stewart <james@ketlai.co.uk>
2010-03-20 19:46:39 +0000

    404 if tag not found

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.first(:conditions => ['id = ? OR name = ?', params[:id], params[:id]])
+    raise ActiveRecord::RecordNotFound unless @tag
+    
     @dreams = Dream.listings.tagged_with(@tag.name, {}).paginate(:page => params[:page], :per_page => 20)
     
     respond_to do |format|
