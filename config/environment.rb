79efd8061d71959a392be25a73d2f78fc6969dc2
James Stewart <james@ketlai.co.uk>
2010-05-30 17:39:22 +0100

    Upgrade delayed_job

--- config/environment.rb
+++ config/environment.rb
@@ -10,10 +10,10 @@ Rails::Initializer.run do |config|
   config.gem 'paperclip', :source => 'http://gemcutter.org', :version => '>=2.3.1'
   config.gem 'clearance', :source => 'http://gemcutter.org', :version => '0.8.4'
   config.gem 'will_paginate', :source => 'http://gemcutter.org', :version => '2.3.12'
-  config.gem 'collectiveidea-delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com', :version => '>=1.8.0'
+  config.gem 'delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com', :version => '>=2.0.0'
 
   config.gem "thinking-sphinx", :lib => 'thinking_sphinx', :source => 'http://gemcutter.org', :version => '>= 1.3.8'
-  config.gem 'ts-delayed-delta', :lib => 'thinking_sphinx/deltas/delayed_delta', :version => '>= 1.0.0', :source  => 'http://gemcutter.org'
+  config.gem 'ts-delayed-delta', :lib => 'thinking_sphinx/deltas/delayed_delta', :version => '>= 1.1.0', :source  => 'http://gemcutter.org'
 
   config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on", :version => '1.0.5'
   config.gem "RedCloth", :lib => 'redcloth'
