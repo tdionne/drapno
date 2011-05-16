e78278e74dc00206bda6b3aae8a7944e3a3ccffe
James Stewart <jys@ketlai.co.uk>
2011-05-16 11:32:39 +0100

    Remove unnecessary InstanceMethods module

--- lib/activity_monitor.rb
+++ lib/activity_monitor.rb
@@ -2,7 +2,6 @@ module ActivityMonitor
   def self.included(klass)
     klass.send(:cattr_accessor, :monitoring_details)
     klass.send(:extend, ClassMethods)
-    klass.send(:include, InstanceMethods)
   end
   
   module ClassMethods
@@ -15,19 +14,17 @@ module ActivityMonitor
     end
   end
   
-  module InstanceMethods
-    def store_activity    
-      params = {:object_type => self.class.to_s, :object_name => to_s, :verb => self.monitoring_details[:verb], :actor_id => dreamer.id, :actor_name => dreamer.display_name}
-      params[:object_id], params[:object_title] = self.monitoring_details[:object_details].call(self)
+  def store_activity    
+    params = {:object_type => self.class.to_s, :object_name => to_s, :verb => self.monitoring_details[:verb], :actor_id => dreamer.id, :actor_name => dreamer.display_name}
+    params[:object_id], params[:object_title] = self.monitoring_details[:object_details].call(self)
 
-      notified = []
-      
-      self.monitoring_details[:parties].each do |proxy, reason|
-        send(proxy).each do |user|
-          unless notified.include?(user.id)
-            Activity.create(params.merge(:for_user_id => user.id, :reason => reason))
-            notified << user.id
-          end
+    notified = []
+    
+    self.monitoring_details[:parties].each do |proxy, reason|
+      send(proxy).each do |user|
+        unless notified.include?(user.id)
+          Activity.create(params.merge(:for_user_id => user.id, :reason => reason))
+          notified << user.id
         end
       end
     end
