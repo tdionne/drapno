ccc4e893520985b80ddb4595bacd8296154f6ded
Timothy <tdionne@salesforce.com>
2016-12-13 08:46:05 -0800

    Fix google_analytics, use html_safe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,16 @@
 module ApplicationHelper
   def google_analytics
     if Rails.env.production?
-      return <<-END
+      return '
         <script type="text/javascript">
         var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
         </script>
         <script type="text/javascript">
         try {
         var pageTracker = _gat._getTracker("UA-11988050-1");
         pageTracker._trackPageview();
-        } catch(err) {}</script>
-      END
+        } catch(err) {}</script>'.html_safe
     end
   end
 end
