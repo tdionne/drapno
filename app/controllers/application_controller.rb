e9862b0f677001a9f5aa50439ff69efde1097933
Timothy <tdionne@salesforce.com>
2016-12-13 17:10:01 -0800

    Fixes login screen, and uses mysql for development.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 require 'clearance/authentication'
+require 'invitation_system'
 
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   include Clearance::Authentication
   # include ErrorRenderers
-  # include InvitationSystem
+  include InvitationSystem
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
