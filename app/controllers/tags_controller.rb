75785833bc278244b6bc80968740feca82e5ebd1
Timothy <tdionne@salesforce.com>
2016-12-17 07:45:39 -0800

    Fixes links to tags.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.first(:conditions => ['id = ? OR name = ?', params[:id], params[:id]])
+    @tag = ActsAsTaggableOn::Tag.first(:conditions => ['id = ? OR name = ?', params[:id], params[:id]])
     raise ActiveRecord::RecordNotFound unless @tag
     
-    @dreams = Dream.listings.tagged_with(@tag.name, {}).paginate(:page => params[:page], :per_page => 20)
+    @dreams = Dream.tagged_with(@tag.name, {}).paginate(:page => params[:page], :per_page => 20)
     
     respond_to do |format|
       format.html
